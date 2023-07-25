6fb7c921373e0615ed3166efdcdeee1e69085cc7
okbandybhw <okbandybhw@gmail.com>
2023-07-26 03:08:30 +0800

    新增點擊辨識物件

---
+++ Assets/Script/InteractInput.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractInput : MonoBehaviour
+{
+    [SerializeField]
+    TMPro.TextMeshProUGUI textOnScreen;
+    InteractableObject hoveringOverObject;
+    void Update()
+    {
+        CheckInteractableObject();
+        if (Input.GetMouseButton(0))
+        {
+            if (hoveringOverObject != null)
+                hoveringOverObject.Interact();
+        }
+    }
+
+    private void CheckInteractableObject()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            var interactableObject = hit.transform.GetComponent<InteractableObject>();
+            if (interactableObject != null)
+            {
+                hoveringOverObject = interactableObject;
+                textOnScreen.text = interactableObject.transform.name;
+            }
+            else
+            {
+                hoveringOverObject = null;
+                textOnScreen.text = "";
+            }
+        }
+    }
+}
