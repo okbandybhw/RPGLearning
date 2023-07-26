75ffc8347229aead93a795ed1cec752b720d72bd
okbandybhw <okbandybhw@gmail.com>
2023-07-27 05:37:35 +0800

    新增腳色能力值和屬性

---
+++ Assets/Script/Character/Character.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CharacterData
+{
+    public class Character : MonoBehaviour
+    {
+        CharacterData characterData;
+
+        void Awake()
+        {
+            characterData = new CharacterData();
+        }
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
--- Assets/Script/Character/CharacterData.cs
+++ Assets/Script/Character/CharacterData.cs
@@ -1,19 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace CharacterData
 {
+    public enum StatsType
+    {
+        Life,
+        Damage,
+        Armor,
+    }
+
+    public class Stats
+    {
+        public StatsType statsType;
+        public int statsValue;
+
+        public Stats(StatsType statsType, int statsValue = 0)
+        {
+            this.statsType = statsType;
+            this.statsValue = statsValue;
+        }
+    }
+
     public enum AttributeType
     {
         Strength,
         Dexterity,
         Intelligence,
-    }
-
-    public class CharacterData
-    {
-        public List<Attribute> attributeValues;
     }
 
     public class Attribute
@@ -25,6 +40,26 @@ namespace CharacterData
         {
             this.attributeType = attributeType;
             this.attributeValue = attributeValue;
+        }
+    }
+
+    public class CharacterData
+    {
+        public List<Attribute> attributeValues;
+        public List<Stats> StatsS;
+
+        public CharacterData()
+        {
+            attributeValues = new List<Attribute>();
+            foreach (AttributeType attributeType in Enum.GetValues(typeof(AttributeType)))
+                attributeValues.Add(new Attribute(attributeType, 0));
+
+            StatsS = new List<Stats>();
+            StatsS.Add(new Stats(StatsType.Life, 100));
+            StatsS.Add(new Stats(StatsType.Damage, 20));
+            StatsS.Add(new Stats(StatsType.Armor, 50));
+            // foreach (StatsType statsType in Enum.GetValues(typeof(StatsType)))
+            //     StatsS.Add(new Stats(statsType));
         }
     }
 }
